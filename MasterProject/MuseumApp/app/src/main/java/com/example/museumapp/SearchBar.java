3e515d31b28add235b9fab4ab9fbf74b9f7688fb
Yujiaa <zuoaurora@gmail.com>
2023-03-15 00:35:37 -0500

    Edit xml
    About searchBar.java, and nav_manu.xml and string.xml

--- MasterProject/MuseumApp/app/src/main/java/com/example/museumapp/SearchBar.java
+++ MasterProject/MuseumApp/app/src/main/java/com/example/museumapp/SearchBar.java
@@ -1,4 +1,13 @@
 package com.example.museumapp;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class SearchBar {
+
+
 }
