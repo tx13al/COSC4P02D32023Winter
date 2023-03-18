586deded5a99c172030985f6c01d7aef8ced0d2e
hl19wl <122480167+hihileung1990@users.noreply.github.com>
2023-03-18 00:24:43 -0400

    Create SQL Command Tester

---
+++ MasterProject/MuseumApp/app/src/main/java/com/example/museumapp/SQLCommandTester.java
@@ -0,0 +1,19 @@
+package com.example.museumapp;
+
+public class SQLCommandTester {
+    String DB_URL;
+    String DB_Name;
+    String DB_User;
+    String DB_Pass;
+    String DB_Port;
+
+    public SQLCommandTester() {
+        DB_URL = BuildConfig.DB_URL;
+        DB_Name = BuildConfig.DB_Name;
+        DB_User = BuildConfig.DB_User;
+        DB_Pass = BuildConfig.DB_Pass;
+        DB_Port = BuildConfig.DB_Port;
+    }
+
+
+}
