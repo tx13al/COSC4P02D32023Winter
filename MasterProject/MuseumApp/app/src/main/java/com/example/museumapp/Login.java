79dbd69b52b39d739f886148746ad8b5979e6c8f
yujiiia <92174428+SYYujiaZ@users.noreply.github.com>
2023-04-04 17:48:34 -0400

    delete something useless

--- MasterProject/MuseumApp/app/src/main/java/com/example/museumapp/Login.java
+++ MasterProject/MuseumApp/app/src/main/java/com/example/museumapp/Login.java
@@ -75,49 +75,6 @@ public class Login  implements View.OnClickListener {
 
                 //TODO
             }
-
-            public void SearchBar(Context context, String[] arr, AutoCompleteTextView View) {
-                //search bar
-                String[] stringArray = arr;
-
-                ArrayAdapter<String> completion = new ArrayAdapter<>(
-                        context, android.R.layout.simple_dropdown_item_1line, arr
-                );
-
-                AutoCompleteTextView actv = View;
-                actv.setAdapter(completion);
-                actv.setThreshold(1);
-                actv.setInputType(InputType.TYPE_CLASS_TEXT);
-
-                actv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                        String selectedItem = (String) adapterView.getItemAtPosition(i);
-                        Toast.makeText(context, "Item "+selectedItem+ " found. ", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                actv.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-                    @Override
-                    public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
-                        if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_NEXT) {
-                            String enteredText = textView.getText().toString();
-                            int position = ((ArrayAdapter<String>) actv.getAdapter()).getPosition(enteredText);
-                            if (position >= 0) {
-                                // The entered text is in the adapter
-                                Toast.makeText(context, "Item "+enteredText+ " found. ", Toast.LENGTH_SHORT).show();
-                            } else {
-                                // The entered text is not in the adapter
-                                Toast.makeText(context, enteredText+" is NOT found", Toast.LENGTH_SHORT).show();
-                            }
-                            return true;
-                        }
-                        return false;
-                    }
-                });
-            }
         });
     }
-
-
 }
