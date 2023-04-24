e29791623a5fab2838e22312ab372278d6b63316
Awaish0419 <hl21bi@brocku.ca>
2023-04-23 23:47:49 -0400

    Add a layout for the item list activity to view all items selected.

--- MasterProject/MuseumApp/app/src/main/java/com/example/museumapp/ItemListActivity.java
+++ MasterProject/MuseumApp/app/src/main/java/com/example/museumapp/ItemListActivity.java
@@ -1,6 +1,13 @@
 package com.example.museumapp;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ItemListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
