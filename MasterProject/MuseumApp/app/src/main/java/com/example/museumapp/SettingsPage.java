e9333f65ab58f47ad09180a245f17c55da9eda97
tx13al <122407805+tx13al@users.noreply.github.com>
2023-04-28 01:44:59 -0400

    update settingsPage class
    remove unused imports

--- MasterProject/MuseumApp/app/src/main/java/com/example/museumapp/SettingsPage.java
+++ MasterProject/MuseumApp/app/src/main/java/com/example/museumapp/SettingsPage.java
@@ -1,18 +1,13 @@
 package com.example.museumapp;
 
-import android.content.Intent;
 import android.content.res.Configuration;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 
 public class SettingsPage extends AppCompatActivity {
